d52f9630a8e8a047b7c69fdc8ab64e182f752b67
Javaer <15297886277@163.com>
2019-04-12 18:41:08 +0800

    some adjust.

--- app/src/main/java/com/superwe/swipeview/TextAdapter.java
+++ app/src/main/java/com/superwe/swipeview/TextAdapter.java
@@ -32,29 +32,36 @@ public class TextAdapter extends BaseListAdapter<TextWord>{
         TextView text = new TextView(getContext());
         text.setId(0);
         LinearLayout.LayoutParams text_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, dp2px(50));
-        text_params.setMargins(dp2px(5), 0, dp2px(5), 0);
+        text_params.setMargins(dp2px(18), 0, dp2px(18), 0);
         text.setGravity(Gravity.CENTER_VERTICAL);
         text.setTextSize(dp2px(6));
         text.getPaint().setFakeBoldText(true);
         text.setLayoutParams(text_params);
 
+        LinearLayout te_layout = new LinearLayout(getContext());
+        te_layout.setId(3);
+        te_layout.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams te_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        te_layout.setLayoutParams(te_params);
+        te_layout.addView(text);
+
         TextView edit = new TextView(getContext());
         LinearLayout.LayoutParams edit_params = new LinearLayout.LayoutParams(dp2px(80), dp2px(50));
         edit.setId(2);
-        edit.setGravity(Gravity.CENTER_VERTICAL);
+        edit.setGravity(Gravity.CENTER);
         edit.setTextSize(dp2px(6));
         edit.getPaint().setFakeBoldText(true);
         edit.setLayoutParams(edit_params);
 
         TextView del = new TextView(getContext());
-        LinearLayout.LayoutParams del_params = new LinearLayout.LayoutParams(dp2px(50), dp2px(50));
+        LinearLayout.LayoutParams del_params = new LinearLayout.LayoutParams(dp2px(60), dp2px(50));
         del.setId(1);
-        del.setGravity(Gravity.CENTER_VERTICAL);
+        del.setGravity(Gravity.CENTER);
         del.setTextSize(dp2px(6));
         del.getPaint().setFakeBoldText(true);
         del.setLayoutParams(del_params);
 
-        swipeMenuLayout.addView(text);
+        swipeMenuLayout.addView(te_layout);
         swipeMenuLayout.addView(edit);
         swipeMenuLayout.addView(del);
         return swipeMenuLayout;
@@ -70,6 +77,7 @@ public class TextAdapter extends BaseListAdapter<TextWord>{
         private TextView text;
         private TextView edit;
         private TextView del;
+        private LinearLayout te_layout;
 
         public TextViewHolder(View view, BaseListAdapter<TextWord> baseListAdapter) {
             super(view, baseListAdapter);
@@ -80,11 +88,12 @@ public class TextAdapter extends BaseListAdapter<TextWord>{
             text = mItemView.findViewById(0);
             del = mItemView.findViewById(1);
             edit = mItemView.findViewById(2);
+            te_layout = mItemView.findViewById(3);
         }
 
         @Override
         public void onBind(final int position, int viewType) {
-            TextWord textWord = getItem(position);
+            final TextWord textWord = getItem(position);
             text.setText(textWord.getText());
             del.setText("删除");
             del.setBackgroundColor(Color.RED);
@@ -101,7 +110,7 @@ public class TextAdapter extends BaseListAdapter<TextWord>{
 
             edit.setText("编辑");
             edit.setTextColor(Color.WHITE);
-            edit.setBackgroundColor(Color.GREEN);
+            edit.setBackgroundColor(Color.parseColor("#7fffd4"));
             edit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -109,6 +118,14 @@ public class TextAdapter extends BaseListAdapter<TextWord>{
                     SwipeMenuLayout swipeMenuLayout = (SwipeMenuLayout) mItemView;
                     swipeMenuLayout.quickClose();
                     Toast.makeText(getContext(), "编辑", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            te_layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "点击:"+position+"===>"+textWord.getText(), Toast.LENGTH_SHORT).show();
+
                 }
             });
         }
